64284cbad600342339500602213e3f844d99bb73
Lucas Abreu <lucasabreu.c18@gmail.com>
2023-05-11 17:33:31 -0300

    "correção"

--- source/scripts/aritimetica.js
+++ source/scripts/aritimetica.js
@@ -4,6 +4,6 @@ const multiplicar = require('./calc');
 const dividir = require('./calc');
 
 console.log('somar: ', somar(10, 20));
-console.log('somar: ', diminuir(3, 40));
-console.log('somar: ', multiplicar(30, 10));
-console.log('somar: ', dividir(50, 2));
+console.log('diminuir: ', diminuir(3, 40));
+console.log('multiplicar: ', multiplicar(30, 10));
+console.log('dividir: ', dividir(50, 2));
